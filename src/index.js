86c350cb5ec356b569f9ba9ea7c141cf6b7d4073
Julie Judachina <iuliia.saunova@email.cz>
2021-07-10 01:00:21 +0200

    task 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './sass/main.scss';
+
+
+const startBTn = document.querySelector('data-start')
+const stopBTn = document.querySelector('data-stop')
+const PROMT_DELAY = 1000
+let timeOutId = null
+let isActive = false;
+
+startBTn.addEventListener('click', startClick)
+startBTn.addEventListener('click', stopClick)
+
+
+
+
+function startClick() {
+    if (isActive) {
+        return;
+    }
+    isActive = true;
+    intervalId = setInterval(changeBodyBgcolor, PROMT_DELAY)
+};
+
+function changeBodyBgcolor() {
+    console.log(document.body.style.background = `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+}
+
+function stopClick() {
+    clearInterval(intervalId);
+    isActive = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
