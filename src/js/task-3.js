c887d623847ebbcf3c33d1f75ac7fd2bfbfb7827
Julie Judachina <iuliia.saunova@email.cz>
2021-07-11 01:24:29 +0200

    Update task-3.js

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -23,17 +23,20 @@ const users = [
     { name: 'Ajax', active: false },
 ];
 
-const toggleUserState = (allUsers, username, callback) => {
-    const updatedUsers = allUsers.map(user =>
-        user.name === username ? { ...user, active: !user.active } : user
-    );
+const toggleUserState = (allUsers, username,) => {
 
-    callback(updatedUsers);
+    return new Promise (resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === username ? { ...user, active: !user.active } : user
+    )
+    resolve (updatedUsers);
+  })
 };
+    
 
 // Currently the function works like this
-toggleUserState(users, 'Mango', console.table);
-toggleUserState(users, 'Ajax', console.table);
+//toggleUserState(users, 'Mango', console.table);
+//toggleUserState(users, 'Ajax', console.table);
 
 // The function should work like this
 toggleUserState(users, 'Mango').then(console.table);
@@ -45,34 +48,34 @@ toggleUserState(users, 'Ajax').then(console.table);
 
 
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+  const makeTransaction = transaction => { 
+  return new Promise ((resolve,reject) => {
     const delay = randomIntegerFromInterval(200, 500);
-
+  
     setTimeout(() => {
-        const canProcess = Math.random() > 0.3;
+      const canProcess = Math.random() > 0.3;
 
-        if (canProcess) {
-            onSuccess({ id: transaction.id, time: delay });
-        } else {
-            onError(transaction.id);
-        }
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      } 
+        reject(transaction.id);
     }, delay);
-};
+  })};
 
 const logSuccess = ({ id, time }) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
+  console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
 const logError = id => {
-    console.warn(`Error processing transaction ${id}. Please try again later.`);
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 // Currently the function works like this
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+//makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+//makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
 
 // The function should work like this
 makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
